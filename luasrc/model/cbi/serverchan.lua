8e129feed81297a71253582d8ab0030c13934aca
tty228 <33397881+tty228@users.noreply.github.com>
2020-01-10 23:14:21 +0800

    增加了ip接口选项以适应多拨环境，新增ipv6

--- luasrc/model/cbi/serverchan.lua
+++ luasrc/model/cbi/serverchan.lua
@@ -5,10 +5,15 @@ local net = require "luci.model.network".init()
 local sys = require "luci.sys"
 local ifaces = sys.net:devices()
 
-m=Map("serverchan",translate("ServerChan"),translate("「Server酱」，英文名「ServerChan」，是一款从服务器推送报警信息和日志到微信的工具。"))
+m=Map("serverchan",translate("ServerChan"),
+translate("「Server酱」，英文名「ServerChan」，是一款从服务器推送报警信息和日志到微信的工具。<br /><br />如果你在使用中遇到问题，请到这里提交：")
+.. [[<a href="https://github.com/tty228/luci-app-serverchan" target="_blank">]]
+.. translate("github 项目地址")
+.. [[</a>]]
+)
+
 
 m:section(SimpleSection).template  = "serverchan/serverchan_status"
-
 s=m:section(NamedSection,"serverchan","serverchan",translate("Server酱设置"))
 s:tab("tab_basic", translate("基本设置"))
 s:tab("tab_basic2", translate("设备状态"))
@@ -51,9 +56,56 @@ end
 e.write=function(e,e,e)
 end
 
-a=s:taboption("tab_basic2", Flag,"serverchan_ip",translate("WAN ip 变化通知"))
-a.default=0
-a.rmempty=true
+a=s:taboption("tab_basic2", ListValue,"serverchan_ipv4",translate("ipv4 变动通知"))
+a.default="disable"
+a:value("0",translate("关闭"))
+a:value("1",translate("通过接口获取"))
+a:value("2",translate("通过URL获取"))
+a = s:taboption("tab_basic2", ListValue, "ipv4_interface", translate("接口名称"))
+a:depends({serverchan_ipv4="1"})
+for _, iface in ipairs(ifaces) do
+	if not (iface == "lo" or iface:match("^ifb.*")) then
+		local nets = net:get_interface(iface)
+		nets = nets and nets:get_networks() or {}
+		for k, v in pairs(nets) do
+			nets[k] = nets[k].sid
+		end
+		nets = table.concat(nets, ",")
+		a:value(iface, ((#nets > 0) and "%s (%s)" % {iface, nets} or iface))
+	end
+end
+a.description = translate("<br/>一般选择 wan 接口，多拨环境请自行选择")
+a= s:taboption("tab_basic2", Value, "ipv4_URL", "URL 地址")
+a.rmempty = true 
+a.default = "members.3322.org/dyndns/getip"
+a:depends({serverchan_ipv4="2"})
+a.description = translate("<br/>会因服务器稳定性/获取频繁等原因导致获取失败，一般不推荐")
+
+a=s:taboption("tab_basic2", ListValue,"serverchan_ipv6",translate("ipv6 变动通知"))
+a.default="disable"
+a:value("0",translate("关闭"))
+a:value("1",translate("通过接口获取"))
+a:value("2",translate("通过URL获取"))
+a = s:taboption("tab_basic2", ListValue, "ipv6_interface", translate("接口名称"))
+a:depends({serverchan_ipv6="1"})
+for _, iface in ipairs(ifaces) do
+	if not (iface == "lo" or iface:match("^ifb.*")) then
+		local nets = net:get_interface(iface)
+		nets = nets and nets:get_networks() or {}
+		for k, v in pairs(nets) do
+			nets[k] = nets[k].sid
+		end
+		nets = table.concat(nets, ",")
+		a:value(iface, ((#nets > 0) and "%s (%s)" % {iface, nets} or iface))
+	end
+end
+a.description = translate("<br/>一般选择 wan 接口，多拨环境请自行选择")
+a= s:taboption("tab_basic2", Value, "ipv6_URL", "URL 地址")
+a.rmempty = true 
+a.default = "ip.sb"
+a:depends({serverchan_ipv6="2"})
+a.description = translate("<br/>会因服务器稳定性/获取频繁等原因导致获取失败，一般不推荐")
+
 a=s:taboption("tab_basic2", Flag,"serverchan_up",translate("设备上线通知"))
 a.default=0
 a.rmempty=true
@@ -186,10 +238,9 @@ for _, iface in ipairs(ifaces) do
 		n:value(iface, ((#nets > 0) and "%s (%s)" % {iface, nets} or iface))
 	end
 end
-
-
 local apply = luci.http.formvalue("cbi.apply")
  if apply then
      io.popen("/etc/init.d/serverchan start")
 end
+
 return m
