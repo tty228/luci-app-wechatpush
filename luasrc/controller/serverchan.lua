0a6f612f3524c1f1f3cbf9f785c457855d98733f
tty228 <tty228@yeah.net>
2020-10-24 12:49:48 +0800

    update

--- luasrc/controller/serverchan.lua
+++ luasrc/controller/serverchan.lua
@@ -13,12 +13,12 @@ function index()
 	entry({"admin", "services", "serverchan", "log"}, form("serverchan/log"),_("日志"), 99).leaf = true
 	entry({"admin", "services", "serverchan", "get_log"}, call("get_log")).leaf = true
 	entry({"admin", "services", "serverchan", "clear_log"}, call("clear_log")).leaf = true
-	entry({"admin","services","serverchan","status"},call("act_status")).leaf=true
+	entry({"admin", "services", "serverchan", "status"}, call("act_status")).leaf=true
 end
 
 function act_status()
 	local e={}
-	e.running=luci.sys.call("pgrep -f serverchan/serverchan >/dev/null")==0
+	e.running=luci.sys.call("pgrep serverchan >/dev/null")==0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
