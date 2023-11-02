c750879e01d5417c9634da71abfd6cb872688914
tty228 <tty228@yeah.net>
2023-11-02 22:21:54 +0800

    增加代理服务器选项
    现在可以使用代理服务器推送信息，可能有助于企业微信可信 IP 设置

--- htdocs/luci-static/resources/view/wechatpush/config.js
+++ htdocs/luci-static/resources/view/wechatpush/config.js
@@ -180,6 +180,12 @@ return view.extend({
 		o.depends('jsonpath', '/usr/share/wechatpush/api/qywx_mpnews.json');
 		o.description = _('Supports JPG and PNG formats within 2MB <br> Optimal size: 900383 or 2.35:1');
 
+		o = s.taboption('basic', form.Value, 'proxy_ip', _('Trusted IP address'));
+		o.rmempty = false;
+		o.depends('jsonpath', '/usr/share/wechatpush/api/qywx_mpnews.json');
+		o.depends('jsonpath', '/usr/share/wechatpush/api/qywx_markdown.json');
+		o.description = _('If the application was created after June 20, 2022, you need to set the trusted IP. This option should be used in conjunction with a proxy server.');
+
 		o = s.taboption('basic', form.Value, 'wxpusher_apptoken', _('appToken'));
 		o.description = _('Get Instructions') + ' <a href="https://wxpusher.zjiecode.com/docs/#/?id=%e5%bf%ab%e9%80%9f%e6%8e%a5%e5%85%a5" target="_blank">' + _('Click here') + '</a>';
 		o.rmempty = false;
@@ -225,7 +231,9 @@ return view.extend({
 		};
 		o.description = _('Please refer to the comments and other interface files for modifications. Limited resources, no longer supporting more interfaces, please debug on your own.<br />You can use a similar website to check the JSON file format: https://www.google.com/search?q=JSON+Parser+Online<br />Please use the 「Save」 button in the text box.');
 		o.depends('jsonpath', '/usr/share/wechatpush/api/diy.json');
-		
+
+		o = s.taboption('basic', form.Value, 'proxy_address', _('Proxy Address'));
+		o.description = _('When you want to use a proxy to push information, you can use this option.<br/>This may be helpful for scenarios like trusted IPs for WeChat Work.Using special characters may cause sending failure.<br/>Example:<br/>http://username:password@127.0.0.1:1080<br/>socks5://username:password@127.0.0.1:1080');
 
 		o = s.taboption('basic', form.Button, '_test', _('Send Test'), _('You may need to save the configuration before sending.'));
 		o.inputstyle = 'add';
